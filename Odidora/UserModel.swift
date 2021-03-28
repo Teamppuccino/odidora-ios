3a6690f57236b306d160b2f39126794e6a4c3581
eunyuni <eunyuuni@gmail.com>
2021-03-28 16:04:02 +0900

    add: user model추가, login view 추가

---
+++ Odidora/UserModel.swift
@@ -0,0 +1,28 @@
+//
+//  UserModel.swift
+//  Odidora
+//
+//  Created by 은영김 on 2021/03/28.
+//
+
+import Foundation
+
+struct UserModel: Codable  {
+    let userId: String
+    let userPw: String
+    let userName: String
+    let userBirth: String
+    let socialYype: String
+    let userImg: String
+    let userToken: String
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case userPw = "user_pw"
+        case userName = "user_name"
+        case userBirth = "user_birth"
+        case socialYype = "social_type"
+        case userImg = "user_img"
+        case userToken = "user_token"
+    }
+}
