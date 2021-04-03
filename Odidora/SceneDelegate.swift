5ddab0babf5f2359ec2127726a8b14a69dd547c7
JISNG <jxsxxng@gmail.com>
2021-04-03 18:23:42 +0900

    Add: 자동로그인

--- Odidora/SceneDelegate.swift
+++ Odidora/SceneDelegate.swift
@@ -21,10 +21,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let id = UserDefaults.standard.string(forKey: "userId") ?? ""
+        let token = UserDefaults.standard.string(forKey: "userToken") ?? ""
+        
+        APIManager.requestLogin(id: id, token: token) { response in
+            self.window = UIWindow(windowScene: windowScene)
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as? UITabBarController
+            self.window?.rootViewController = initialViewController
+            self.window?.backgroundColor = .systemBackground
+            self.window?.makeKeyAndVisible()
+        }
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
